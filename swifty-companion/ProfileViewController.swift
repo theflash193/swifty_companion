82257153af8860e0a6986e33eef9f149f16a3fb4
Gordon RASS-KWASI <grass-kw@e2r1p7.42.fr>
2016-08-25 16:37:39 +0200

    mise en pag profile

--- swifty-companion/ProfileViewController.swift
+++ swifty-companion/ProfileViewController.swift
@@ -12,19 +12,25 @@ class ProfileViewController: UIViewController {
     
     var student: Student!
     
+    @IBOutlet weak var photoProfile: UIImageView!
     @IBOutlet weak var login: UILabel!
-//    @IBOutlet weak var level: UILabel!
-//    @IBOutlet weak var promotion: UILabel!
-//    @IBOutlet weak var email: UILabel!
-//    @IBOutlet weak var phone: UILabel!
+    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var promotion: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    
     
     override func viewWillAppear(animated: Bool) {
         navigationController?.title = "Profile"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        photoProfile.image = student.photo
         login.text = student!.login
+        level.text = student!.niveau
+        promotion.text = student!.promotion
+        email.text = student!.email
+        phone.text = student!.mobile
     }
     
     override func didReceiveMemoryWarning() {
