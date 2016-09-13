1a44bb065db4f96ba79c9634cd6d5dd90e52f2dc
Gordon RASS-KWASI <grass-kw@e3r9p3.42.fr>
2016-09-13 02:51:41 +0200

    recuperation du niveau selon la promotion

--- swifty-companion/Student.swift
+++ swifty-companion/Student.swift
@@ -27,19 +27,20 @@ class Student {
     init?(data: JSON) {
         
         let url = NSURL(string: data["image_url"].stringValue)
-//        let skills: JSON = data["cursus_users"][0]["skill"]
-//        print(skills)
-//        for (key,subJson):(String, JSON) in skills {
-//            print("key : \(key), subJson : \(subJson)")
-//        }
-//        print(data["cursus_users"][0]["skills"])
         self.photo = UIImage(data: NSData(contentsOfURL: url!)!)
         self.login = data["login"].stringValue
         self.full_name = data["displayname"].stringValue
         self.email = data["email"].stringValue
         self.mobile = data["phone"].stringValue
         self.promotion = data["pool_year"].stringValue
-        self.niveau = data["cursus_users"][0]["level"].stringValue
+        
+        var student_level = ""
+        for (_, cursus) in data["cursus_users"] {
+            if cursus["cursus"]["name"].stringValue == "42" {
+                student_level = cursus["level"].stringValue
+            }
+        }
+        self.niveau = student_level
         self.competence = data["cursus_users"][0]["skills"]
         self.projets = data["projects_users"]
     }
