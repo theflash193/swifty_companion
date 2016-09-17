4321eb313ca86dd29727fa7ac0c1bbaa264ecad6
Gordon RASS-KWASI <grass-kw@e1r2p16.42.fr>
2016-09-17 15:32:59 +0200

    affichage dans une table view

--- Swift_companion/AchievementViewController.swift
+++ Swift_companion/AchievementViewController.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class AchievementViewController: UIViewController {
+class AchievementViewController: UIViewController, UITableViewDataSource {
     var student: Student?
+    // MARK: - Attributs
+    @IBOutlet weak var TableView: UITableView!
     
     override func viewWillAppear(animated: Bool) {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableView.dataSource = self
+        print(student?.achievements)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    // MARK: - DataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if student!.achievements != nil {
+            return student!.achievements!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let myCell: UITableViewCell = TableView.dequeueReusableCellWithIdentifier("Achievement", forIndexPath: indexPath)
+        
+        if student!.achievements != nil {
+            let detailAchievement: String = student!.competence![indexPath.row]["name"].stringValue + " detail:" + student!.achievements![indexPath.row]["description"].stringValue
+            myCell.textLabel?.text = detailAchievement
+        } else {
+            print("not found")
+        }
+        return myCell
+    }
+
 }
